248ed32047e9f8b7c6a88b9392574eb957b0fe18
Aaron Foley <aaron.s.foley@gmail.com>
2016-01-10 14:42:41 +1100

    Fixed zsh key binding for windows terminals

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -4,7 +4,13 @@
 
 bindkey -v   # Default to standard vi bindings, regardless of editor string
 
-bindkey "^[[2~" overwrite-mode      # insert
-bindkey "^[[3~" delete-char         # delete
-bindkey "^[[7~" beginning-of-line   # home
-bindkey "^[[8~" end-of-line         # end
+bindkey "^[[2~" overwrite-mode
+bindkey "^[[3~" delete-char
+bindkey "^[[5~" up-line-or-search
+bindkey "^[[6~" down-line-or-search
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[7~" beginning-of-line
+bindkey "^[[4~" end-of-line
+bindkey "^[[8~" end-of-line
+bindkey "^?" backward-delete-char
+bindkey '^R' history-incremental-search-backward
