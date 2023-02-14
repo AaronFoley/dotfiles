93a78e23ff85c48fca3ff6b3dced791d032d1a64
Aaron Foley <aaron.s.foley@gmail.com>
2023-02-14 14:21:05 +1100

    Changes from laptop

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -8,10 +8,12 @@ bindkey "^[[2~" overwrite-mode
 bindkey "^[[3~" delete-char
 bindkey "^[[5~" up-line-or-search
 bindkey "^[[6~" down-line-or-search
+bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
 bindkey "^[[7~" beginning-of-line
 bindkey "^[[4~" end-of-line
 bindkey "^[[8~" end-of-line
+bindkey "^[[F" end-of-line
 bindkey "^?" backward-delete-char
 bindkey '^R' history-incremental-search-backward
 
